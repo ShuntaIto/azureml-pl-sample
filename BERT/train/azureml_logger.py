d277e0adbb2617c0a873f6005f7b1c60831da541
shunta_ito <shunta.ito@microsoft.com>
2022-01-15 16:09:40 +0000

    Add azureml_logger (experimental)

---
+++ BERT/train/azureml_logger.py
@@ -0,0 +1,52 @@
+from pytorch_lightning.utilities import rank_zero_only
+from pytorch_lightning.loggers import LightningLoggerBase
+from pytorch_lightning.loggers.base import rank_zero_experiment
+
+from azureml.core import Run
+
+class AzureMachineLearningLogger(LightningLoggerBase):
+    def __init__(self):
+        pass
+
+    @property
+    def name(self):
+        return "AzureMachineLearningLogger"
+
+    @property
+    @rank_zero_experiment
+    def experiment(self):
+        # Return the experiment object associated with this logger.
+        run = Run.get_context()
+        return run
+
+    @property
+    def version(self):
+        # Return the experiment version, int or str.
+        run = self.experiment()
+        return run.id
+
+    @rank_zero_only
+    def log_hyperparams(self, params):
+        # params is an argparse.Namespace
+        # your code to record hyperparameters goes here
+        pass
+
+    @rank_zero_only
+    def log_metrics(self, metrics, step):
+        # metrics is a dictionary of metric names and values
+        # your code to record metrics goes here
+        run = self.experiment()
+        for k, v in {**{"step": step}, **metrics}.items():
+            run.log(k, v)
+
+    # @rank_zero_only
+    # def save(self):
+    #     # Optional. Any code necessary to save logger data goes here
+    #     # If you implement this, remember to call `super().save()`
+    #     # at the start of the method (important for aggregation of metrics)
+    #     super().save()
+
+    # @rank_zero_only
+    # def finalize(self, status):
+    #     # Optional. Any code that needs to be run after training
+    #     # finishes goes here
