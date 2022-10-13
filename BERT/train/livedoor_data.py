dbb03bf7cecb90478a20e7a6a27e5a2a4e2da770
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-13 21:43:44 +0000

    Adding tarfile member sanitization to extractall()

--- BERT/train/livedoor_data.py
+++ BERT/train/livedoor_data.py
@@ -23,7 +23,26 @@ def download_dataset():
         urllib.request.urlretrieve(
             "https://www.rondhuit.com/download/ldcc-20140209.tar.gz", download_path)
     with tarfile.open(download_path) as t:
-        t.extractall(extract_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(t, extract_path)
 
 def preprocess():
     ## https://qiita.com/m__k/items/841950a57a0d7ff05506
